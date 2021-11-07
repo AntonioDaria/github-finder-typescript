c68e92c9f3bc1d81bd6ffc13975416ff0015245f
Antonio D'aria <antoniodaria@msn.com>
2021-11-07 10:52:53 +0000

    api running with healthcheck endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,15 @@
 import express from "express";
 import config from "config";
+import routes from "./routes";
 
-// import routes from "./routes";
-
-const port = config.get<number>("port");
-
+//const port = config.get<number>("port");
+const port = 1337;
 const app = express();
 
 app.use(express.json());
-
 
 app.listen(port, async () => {
   console.log(`App is running at http://localhost:${port}`);
 
   routes(app);
-
 });
