fd6a6eb0c2e2d5bc1151f0873eb2d2a1b42bf8b8
Antonio D'aria <antoniodaria@msn.com>
2021-11-07 22:30:18 +0000

    routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,11 @@
 import { Express, Request, Response } from "express";
+import { getFavLanguageController } from "./controller/language.controller";
 
 function routes(app: Express) {
   app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200));
+
+  app.get("/api/languages/:userName/", getFavLanguageController);
+  
 }
 
 export default routes;
